7983ad810b2e31f544dc082c7e5c4f7a01c4f34a
Kirill Smirnov <kirill@dg-line.ru>
2020-09-08 11:41:46 +0300

    init

---
+++ Assets/Scripts/NullCheckTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+using static UnityUtils.MonoBehaviourNullCheck;
+
+namespace DefaultNamespace
+{
+    public class NullCheckTest : MonoBehaviour
+    {
+        [SerializeField] private Camera cam;
+        public OtherScript otherScript;
+
+        private void OnValidate() => CheckNullFields(this);
+    }
+}
