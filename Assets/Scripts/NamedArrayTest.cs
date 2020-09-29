26ec61aa9a5ff7211d4385130c6c6cd88c76a384
Kirill Smirnov <kirill@dg-line.ru>
2020-09-29 11:26:32 +0300

    Add NamedArray attribute

---
+++ Assets/Scripts/NamedArrayTest.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityUtils.Attributes;
+
+public class NamedArrayTest : MonoBehaviour
+{
+    [NamedArray(typeof(SomeEnum))] public int[] data;
+    private enum SomeEnum { A, B, C, D }
+}
