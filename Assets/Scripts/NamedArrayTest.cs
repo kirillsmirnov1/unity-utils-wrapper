e3208d64571a106b52245978ca91814323d232c8
Kirill Smirnov <kirill@dg-line.ru>
2020-10-26 12:06:31 +0300

    some changes for readme

--- Assets/Scripts/NamedArrayTest.cs
+++ Assets/Scripts/NamedArrayTest.cs
@@ -3,6 +3,6 @@ using UnityUtils.Attributes;
 
 public class NamedArrayTest : MonoBehaviour
 {
-    [NamedArray(typeof(SomeEnum))] public int[] data;
-    private enum SomeEnum { A, B, C, D }
+    [NamedArray(typeof(Fruits))] public int[] fruitCount;
+    private enum Fruits { Apple, Banana, Coconut, Duran }
 }
