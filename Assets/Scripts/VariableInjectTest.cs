f7b3da278f7638f536e316492b44211941e7ad0f
Kirill Smirnov <thegaldren@gmail.com>
2021-03-17 02:08:54 +0300

    VariableInjectTest.cs on EventTestScene.unity

---
+++ Assets/Scripts/VariableInjectTest.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityUtils.Variables;
+
+namespace DefaultNamespace
+{
+    public class VariableInjectTest : MonoBehaviour
+    {
+        [SerializeField] private CanvasVariable canvasVariable;
+
+        private void Awake() => TryToCreateAnObjectOnInjectedCanvas();
+
+        private void TryToCreateAnObjectOnInjectedCanvas()
+        {
+            var createdObject = new GameObject();
+            createdObject.transform.parent = canvasVariable.Value.transform;
+            createdObject.AddComponent<RectTransform>().anchoredPosition = Vector2.zero;
+            var textOnObject = createdObject.AddComponent<TextMeshProUGUI>();
+            textOnObject.text = "DI FTW";
+            textOnObject.color = Color.red;
+        }
+    }
+}
