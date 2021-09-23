273c5121b18069939ff5d827dc10875fce986472
Kirill Smirnov <thegaldren@gmail.com>
2021-09-23 13:51:34 +0300

    Observable.cs

---
+++ Assets/Scripts/ObservableTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityUtils;
+
+namespace DefaultNamespace
+{
+    public class ObservableTest : MonoBehaviour
+    {
+        [SerializeField] private Observable<int> intObs;
+        [SerializeField] private Observable<string> stringObs;
+        [SerializeField] private Observable<Person[]> personsObservable;
+        [SerializeField] private Observable<Person>[] personObservables;
+        
+        [Serializable]
+        public class Person
+        {
+            public bool alive;
+            public string name;
+            public DateTime dateOfBirth;
+            public string favouriteIceCreamFlavor;
+        }
+    }
+}
